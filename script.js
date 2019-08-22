50c38cfe5988a1b07777854808717a0ca2ab0d2b
NicoForslund <nicofors2015@gmail.com>
2019-08-22 08:57:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let input = document.getElementById("name");
+
+let button = document.getElementById("button");
+
+
+button.addEventListener('click', onButtonClick);
+
+
+function onButtonClick() {
+
+    alert("Hello " + input.value);
+
+}
